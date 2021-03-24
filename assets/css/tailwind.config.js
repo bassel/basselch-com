2c1b88e6ef1fd03ec1dc8201130bf8688eb9d3d2
Bassel Cheaib <bassel.bc@gmail.com>
2021-03-24 17:55:23 +0200

    Base pages

--- assets/css/tailwind.config.js
+++ assets/css/tailwind.config.js
@@ -1,8 +1,88 @@
 module.exports = {
+  darkMode: 'class',
   theme: {
-    extend: {}
+    extend: {
+      fontSize: {
+        'md': '.925rem',
+      },
+      transitionProperty: {
+        'border': 'border',
+      },
+      colors: {
+        gray: {
+          950: '#080b12'
+        },
+      },
+      typography: (theme) => ({
+        dark: {
+          css: [
+            {
+              color: theme('colors.gray.300'),
+              '[class~="lead"]': {
+                color: theme('colors.gray.200'),
+              },
+              a: {
+                color: theme('colors.white'),
+              },
+              strong: {
+                color: theme('colors.white'),
+              },
+              'ol > li::before': {
+                color: theme('colors.gray.300'),
+              },
+              'ul > li::before': {
+                backgroundColor: theme('colors.gray.500'),
+              },
+              hr: {
+                borderColor: theme('colors.gray.200'),
+              },
+              blockquote: {
+                color: theme('colors.gray.200'),
+                borderLeftColor: theme('colors.gray.500'),
+              },
+              h1: {
+                color: theme('colors.white'),
+              },
+              h2: {
+                color: theme('colors.white'),
+              },
+              h3: {
+                color: theme('colors.white'),
+              },
+              h4: {
+                color: theme('colors.white'),
+              },
+              'figure figcaption': {
+                color: theme('colors.gray.300'),
+              },
+              code: {
+                color: theme('colors.white'),
+              },
+              'a code': {
+                color: theme('colors.white'),
+              },
+              pre: {
+                color: theme('colors.gray.200'),
+                backgroundColor: theme('colors.gray.800'),
+              },
+              thead: {
+                color: theme('colors.white'),
+                borderBottomColor: theme('colors.gray.400'),
+              },
+              'tbody tr': {
+                borderBottomColor: theme('colors.gray.600'),
+              },
+            },
+          ],
+        },
+      }),
+    },
   },
-  variants: {},
+  variants: {
+    extend: {
+      typography: ['dark'],
+    },
+  },
   plugins: [
     require('@tailwindcss/typography'),
   ]
