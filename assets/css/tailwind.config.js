ef8554dd419a9ea566458fe3cbcddae49b943ace
Bassel Cheaib <bassel.bc@gmail.com>
2021-04-12 12:47:08 +0300

    Dependencies update

--- assets/css/tailwind.config.js
+++ assets/css/tailwind.config.js
@@ -1,20 +1,34 @@
-const colors = require('tailwindcss/colors');
-const defaultTheme = require('tailwindcss/defaultTheme');
+const themeDir = __dirname + "/../../";
+const colors = require("tailwindcss/colors");
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 const round = (num) =>
   num
     .toFixed(7)
-    .replace(/(\.[0-9]+?)0+$/, '$1')
-    .replace(/\.0$/, '')
-const rem = (px) => `${round(px / 16)}rem`
-const em = (px, base) => `${round(px / base)}em`
+    .replace(/(\.[0-9]+?)0+$/, "$1")
+    .replace(/\.0$/, "");
+const rem = (px) => `${round(px / 16)}rem`;
+const em = (px, base) => `${round(px / base)}em`;
 
 module.exports = {
-  darkMode: 'class',
+  // mode: "jit",
+  darkMode: "class",
+  purge: {
+    enabled: process.env.HUGO_ENVIRONMENT === "production",
+    content: [
+      "./hugo_stats.json",
+      themeDir + "/hugo_stats.json",
+      themeDir + "/safelist.txt",
+    ],
+    defaultExtractor: (content) => {
+      let els = JSON.parse(content).htmlElements;
+      return els.tags.concat(els.classes, els.ids);
+    },
+  },
   theme: {
     colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
+      transparent: "transparent",
+      current: "currentColor",
       blueGray: colors.blueGray,
       black: colors.black,
       white: colors.white,
@@ -24,52 +38,52 @@ module.exports = {
     },
     extend: {
       fontSize: {
-        'md': '.925rem',
+        md: ".925rem",
       },
       transitionProperty: {
-        'border': 'border',
+        border: "border",
       },
       colors: {
         gray: {
-          '950': '#222222',
+          950: "#222222",
         },
       },
       typography: (theme) => ({
         DEFAULT: {
           css: {
-            color: theme('colors.gray.900'),
+            color: theme("colors.gray.900"),
             '[class~="lead"]': {
-              color: theme('colors.blueGray.700'),
+              color: theme("colors.blueGray.700"),
             },
             a: {
-              textDecoration: 'none',
-              '&:hover': {
-                textDecoration: 'underline'
-              }
+              textDecoration: "none",
+              "&:hover": {
+                textDecoration: "underline",
+              },
             },
-            'code::before': {
-              content: '""'
+            "code::before": {
+              content: '""',
             },
-            'code::after': {
-              content: '""'
-            }
-          }
+            "code::after": {
+              content: '""',
+            },
+          },
         },
         rtl: {
           css: [
             {
               blockquote: {
-                borderLeftWidth: '0',
-                borderLeftColor: 'none',
-                borderRightWidth: '0.25rem',
+                borderLeftWidth: "0",
+                borderLeftColor: "none",
+                borderRightWidth: "0.25rem",
                 borderRightColor: defaultTheme.colors.gray[300],
               },
               table: {
-                textAlign: 'right',
+                textAlign: "right",
               },
-              'ol > li::before': {
+              "ol > li::before": {
                 content: 'counter(list-counter, arabic-indic) "."',
-                position: 'absolute',
+                position: "absolute",
               },
             },
             {
@@ -77,37 +91,37 @@ module.exports = {
                 paddingLeft: 0,
                 paddingRight: em(20, 20),
               },
-              'ol > li': {
+              "ol > li": {
                 paddingLeft: 0,
                 paddingRight: em(28, 16),
               },
-              'ol > li::before': {
-                right: '0',
-                left: 'auto',
+              "ol > li::before": {
+                right: "0",
+                left: "auto",
               },
-              'ul > li': {
+              "ul > li": {
                 paddingLeft: 0,
                 paddingRight: em(28, 16),
               },
-              'ul > li::before': {
+              "ul > li::before": {
                 right: em(4, 16),
-                left: 'auto',
+                left: "auto",
               },
-              'thead th:first-child': {
-                paddingLeft: 'auto',
-                paddingRight: '0',
+              "thead th:first-child": {
+                paddingLeft: "auto",
+                paddingRight: "0",
               },
-              'thead th:last-child': {
-                paddingRight: 'auto',
-                paddingLeft: '0',
+              "thead th:last-child": {
+                paddingRight: "auto",
+                paddingLeft: "0",
               },
-              'tbody td:first-child': {
-                paddingLeft: 'auto',
-                paddingRight: '0',
+              "tbody td:first-child": {
+                paddingLeft: "auto",
+                paddingRight: "0",
               },
-              'tbody td:last-child': {
-                paddingRight: 'auto',
-                paddingLeft: '0',
+              "tbody td:last-child": {
+                paddingRight: "auto",
+                paddingLeft: "0",
               },
             },
           ],
@@ -115,60 +129,60 @@ module.exports = {
         dark: {
           css: [
             {
-              color: theme('colors.gray.200'),
+              color: theme("colors.gray.200"),
               '[class~="lead"]': {
-                color: theme('colors.gray.200'),
+                color: theme("colors.gray.200"),
               },
               a: {
-                color: theme('colors.blue.400'),
+                color: theme("colors.blue.400"),
               },
               strong: {
-                color: theme('colors.white'),
+                color: theme("colors.white"),
               },
-              'ol > li::before': {
-                color: theme('colors.gray.300'),
+              "ol > li::before": {
+                color: theme("colors.gray.300"),
               },
-              'ul > li::before': {
-                backgroundColor: theme('colors.gray.500'),
+              "ul > li::before": {
+                backgroundColor: theme("colors.gray.500"),
               },
               hr: {
-                borderColor: theme('colors.gray.200'),
+                borderColor: theme("colors.gray.200"),
               },
               blockquote: {
-                color: theme('colors.gray.200'),
-                borderLeftColor: theme('colors.gray.500'),
+                color: theme("colors.gray.200"),
+                borderLeftColor: theme("colors.gray.500"),
               },
               h1: {
-                color: theme('colors.white'),
+                color: theme("colors.white"),
               },
               h2: {
-                color: theme('colors.white'),
+                color: theme("colors.white"),
               },
               h3: {
-                color: theme('colors.white'),
+                color: theme("colors.white"),
               },
               h4: {
-                color: theme('colors.white'),
+                color: theme("colors.white"),
               },
-              'figure figcaption': {
-                color: theme('colors.gray.300'),
+              "figure figcaption": {
+                color: theme("colors.gray.300"),
               },
               code: {
-                color: theme('colors.white'),
+                color: theme("colors.white"),
               },
-              'a code': {
-                color: theme('colors.white'),
+              "a code": {
+                color: theme("colors.white"),
               },
               pre: {
-                color: theme('colors.gray.200'),
-                backgroundColor: theme('colors.gray.800'),
+                color: theme("colors.gray.200"),
+                backgroundColor: theme("colors.gray.800"),
               },
               thead: {
-                color: theme('colors.white'),
-                borderBottomColor: theme('colors.gray.400'),
+                color: theme("colors.white"),
+                borderBottomColor: theme("colors.gray.400"),
               },
-              'tbody tr': {
-                borderBottomColor: theme('colors.gray.600'),
+              "tbody tr": {
+                borderBottomColor: theme("colors.gray.600"),
               },
             },
           ],
@@ -178,16 +192,13 @@ module.exports = {
   },
   variants: {
     extend: {
-      display: ['dark'],
-      typography: ['dark', 'direction'],
-      inset: ['direction'],
-      padding: ['direction'],
-      margin: ['direction'],
-      borderWidth: ['direction'],
+      display: ["dark"],
+      typography: ["dark", "direction"],
+      inset: ["direction"],
+      padding: ["direction"],
+      margin: ["direction"],
+      borderWidth: ["direction"],
     },
   },
-  plugins: [
-    require('@tailwindcss/typography'),
-    require('tailwindcss-dir')(),
-  ]
-}
+  plugins: [require("@tailwindcss/typography"), require("tailwindcss-dir")()],
+};
